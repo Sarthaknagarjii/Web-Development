ba1887ed81217642b42789561274afa2544446c6
Sarthaknagarjii <sarthaknagarjii@gmail.com>
2024-01-12 12:48:33 +0530

    Starting with Backend

---
+++ Backend/script.js
@@ -0,0 +1,13 @@
+// let n =5;
+// for(let i=0; i<n; i++){
+//     console.log("Hello",i); 
+
+// }
+
+
+/* # Here now we use anotther file funciton (math.js)
+   in this file with the help of module.export in node */
+
+const math = require("./math");
+
+console.log(math);
